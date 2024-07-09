7e2263d3c978bd080f28bec2a2b7ae9b30000568
guimaraesguii <guifereler@gmail.com>
2024-07-09 03:27:26 -0300

    first lesson

---
+++ src/main/java/com/rockseat/planner/trip/TripController.java
@@ -0,0 +1,40 @@
+package com.rockseat.planner.trip;
+
+
+import com.rockseat.planner.trip.partipant.ParticipantService;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/trips")
+public class TripController {
+    @Autowired
+    private ParticipantService participantService;
+
+    @Autowired
+    private TripRepository repository;
+
+    @PostMapping
+    public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload){
+        Trip newTrip = new Trip(payload);
+
+        this.repository.save(newTrip);
+
+        this.participantService.registerParticipantsToEvent(payload.emails_to_envite(), newTrip.getId());
+
+        return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id){
+        Optional<Trip> trip = repository.findById(id);
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+}
